8286380397ab419f2b4b158209d348f0a5759934
sahildhingra221 <sahildhingra221@gmail.com>
2018-12-04 14:02:05 +0530

    --
    --

---
+++ script.js
@@ -0,0 +1,19 @@
+$(function(){
+  
+  $('.animate-lable .input-group input').focusout(function(){
+    
+    var text_val = $(this).val();
+    
+    if(text_val === "") {
+      
+      $(this).removeClass('has-value');
+      
+    } else {
+      
+      $(this).addClass('has-value');
+      
+    }
+    
+  });
+  
+});
